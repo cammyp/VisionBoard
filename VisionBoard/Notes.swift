3398f1d25b2b23af3b28e732b387c327e0413d4b
Cameron Partee <cp3082@nyu.edu>
2020-01-16 16:41:12 -0500

    made final changes for phase one and added documentation files of my progress

--- VisionBoard/Notes.swift
+++ VisionBoard/Notes.swift
@@ -11,6 +11,15 @@
  addBtn
  tap to add new btn
  
- need to store all buttons with properties yeah?
+ Finished phase one
+ next..
+ 
+ 1 - delete a picture
+ 2 - add onboarding screens with explanations
+ 3 - save board
+ 4 - add background color
+ 5 - add text
+ 6 - make app icon
+ 
  
  */
